9d9a703912d8756d8dcd8eba7d514124cc9f3d26
bojkomatias <55543631+bojkomatias@users.noreply.github.com>
2021-05-25 21:55:31 -0300

    initial commit

--- src/App.svelte
+++ src/App.svelte
@@ -1,48 +1,17 @@
 <script>
   import { onMount } from "svelte";
-  export let date;
+  export let collection;
 
   onMount(async () => {
-    const res = await fetch("/api/date");
-    const newDate = await res.text();
-    date = newDate;
+    const res = await fetch("https://strong-traveling-panther.glitch.me//");
+    const response = await res.json();
+    collection = response.data;
   });
 </script>
 
 <main>
-  <h1>Svelte + Node.js API</h1>
-  <h2>
-    Deployed with
-    <a href="https://vercel.com/docs" target="_blank" rel="noreferrer noopener">
-      Vercel
-    </a>
-    !
-  </h2>
-  <p>
-    <a
-      href="https://github.com/vercel/vercel/tree/main/examples/svelte"
-      target="_blank"
-      rel="noreferrer noopener">
-      This project
-    </a>
-    is a
-    <a href="https://svelte.dev/">Svelte</a>
-    app with three directories,
-    <code>/public</code>
-    for static assets,
-    <code>/src</code>
-    for components and content, and
-    <code>/api</code>
-    which contains a serverless
-    <a href="https://nodejs.org/en/">Node.js</a>
-    function. See
-    <a href="/api/date">
-      <code>api/date</code>
-      for the Date API with Node.js
-    </a>
-    .
-  </p>
-  <br />
+  <h1>CRM de Algo, para Software 3 ...</h1>
+
   <h2>The date according to Node.js is:</h2>
-  <p>{date ? date : 'Loading date...'}</p>
+  <p>{collection ? collection?.productName : "Loading date..."}</p>
 </main>
